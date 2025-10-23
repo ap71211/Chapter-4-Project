6188bb6d70dfec154674a035fb95caa5c0933338
ap71211 <ap71211@email.vccs.edu>
2025-10-23 14:51:46 -0400

    Complete code for geometry calculator

--- Chapter 4 Project.cpp
+++ Chapter 4 Project.cpp
@@ -1,51 +1,101 @@
 // Chapter 4 Projects.cpp : This file contains the main Project for Chapter 4. Branches will be createed from this Project.
-// Project 2: Write a program that asks for the weight of the package and the distance it is to be shipped, then display the charges.
-//
+// Project 3: This program will contain a geometry calculator that can compute the area of a few shapes. 
+// The program will dipsplay a menu and the program will be using switch statements to select the desired shape. 
+
 
 #include <iostream> 
+#include <cmath>
 #include <iomanip>
 using namespace std;
 
 int main()
 
 {
-	double weight = 0.0;
-	double distance = 0.0;
-	double cost = 0.0;
+	double radius = 0;
+	double length;
+	double width;
+	double base;
+	double height = 0.0;
+	double area; 
+	int choice;
 
-	cout << "Enter the weight of the package can only be between 0 and 20 kg: ";
-	cin >> weight;
-
-	cout << "Enter the distance to be shipped can only be between 0 and 3000 miles: ";
-	cin >> distance;
-
-	bool input = (weight <= 0 || weight > 20 || distance <= 0 || distance > 3000);
-	if (input)
-	{
-		cout << "Invalid input. Weight must be between 0 and 20 kg, and distance must be between 0 and 3000 miles." << endl;
-		return 0;
-	}
-
-	if (weight <= 2) {
-
-
-		cost = 1.10;
-	}
-	else if (weight > 2 and weight >= 6) {
-		cost = 2.20;
-	}
-	
-	else if (weight > 6 and weight >= 10) {
-		cost = 3.70;
-	}
-	
-	else if (weight > 10 and weight <= 20) {
-		cost = 4.80;
-	}
-	
-	double totalCost = (distance / 500) * cost;
 	cout << fixed << setprecision(2);
-	cout << "The shipping cost is: $" << totalCost << endl;
+	cout << "Geometry Calculator\n";
+	cout << endl;
+	cout << "1. Calculate the Area of a Circle\n";
+	cout << endl;
+	cout << "2. Calculate the Area of a Rectangle\n";
+	cout << endl;
+	cout << "3. Calculate the Area of a Triangle\n";
+	cout << endl;
+	cout << "4. Quit\n";
+	cout << endl;
+	cout << "Please enter your choice (1-4): ";
+	cin >> choice;
+	cout << endl;
+
+	
+	switch (choice)
+	{
+	case 1:
+		cout << "Enter the radius of the circle: ";
+		cin >> radius;
+		if (radius < 0)
+		{
+			cout << " Radius cannot be negative. Please run the program again and enter a valid radius. \n";
+			return 0;
+		}
+		else if (radius >= 0); {
+
+			area = 3.14159 * pow(radius, 2);
+			cout << "The area of the circle is " << area << endl;
+		}
+		break;
+
+	case 2:
+		cout << "Enter the length of the rectangle: ";
+		cin >> length;
+		cout << "Enter the width of the rectangle: ";
+		cin >> width;
+		if (length < 0 || width < 0)
+		{
+			cout << " Length and width cannot be negative. Please run the program again and enter valid numbers. \n";
+			return 0;
+		}
+		else if (length >= 0 || width >= 0); {
+
+
+			area = length * width;
+			cout << "The area of the rectangle is " << area << endl;
+		}
+		break;
+	case 3:
+		cout << " Enter the length of the traingle's base: ";
+		cin >> base;
+		cout << " Enter the height of the traingle: ";
+		cin >> height;
+		if (base < 0 || height < 0)
+		{
+			cout << " Base and height cannot be negative. Please run the program again and enter valid numbers. \n";
+			return 0;
+		}
+		else if (base >= 0 || height >= 0); {
+			area = base * height * 0.5;
+			cout << " The area of the triangle is " << area << endl;
+		}
+		break;
+	case 4:
+		cout << " Thank you for using the Geometry Calculator. Goodbye!" << endl;
+
+		break;
+	default:
+		if (choice < 1 || choice > 4)
+		{
+			cout << " Invalid choice. Please run the program again and select a valid option (1-4).\n";
+		}
+	}
+
+
 	return 0;
 }
 
