28e809dd977658b80803e982d5fcbd8dc8da0c1e
ap71211 <ap71211@email.vccs.edu>
2025-10-23 13:24:02 -0400

    Completed program for Shipping costs.

--- Chapter 4 Project.cpp
+++ Chapter 4 Project.cpp
@@ -1,44 +1,55 @@
 // Chapter 4 Projects.cpp : This file contains the main Project for Chapter 4. Branches will be createed from this Project.
-// Project 1: Days in a Month. This Program will let the user enter an integer in the range of 1-12 and also ask for the year. Then display the number of days in that month, taking into account leap years for the year. 
+// Project 2: Write a program that asks for the weight of the package and the distance it is to be shipped, then display the charges.
 //
 
 #include <iostream> 
+#include <iomanip>
 using namespace std;
 
-int month;
-int year;
+int main()
 
-
-int main() // Project 1: Days in a Month
 {
-	cout << " Enter a month (1-12): \n";
-	if (!(cin >> month)) return 0;
-	cout << "Enter a year: \n";
-	if (!(cin >> year)) return 0;
+	double weight = 0.0;
+	double distance = 0.0;
+	double cost = 0.0;
 
-	if (month < 1 || month > 12) {
-		cout << "Invalid month. Please enter a number between 1 and 12.\n";
+	cout << "Enter the weight of the package can only be between 0 and 20 kg: ";
+	cin >> weight;
+
+	cout << "Enter the distance to be shipped can only be between 0 and 3000 miles: ";
+	cin >> distance;
+
+	bool input = (weight <= 0 || weight > 20 || distance <= 0 || distance > 3000);
+	if (input)
+	{
+		cout << "Invalid input. Weight must be between 0 and 20 kg, and distance must be between 0 and 3000 miles." << endl;
 		return 0;
 	}
 
+	if (weight <= 2) {
 
 
-	bool leap = ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)); // Check for leap year
-	if (month == 2) {
-		cout << "February has " << (leap ? 29 : 28) << " days in " << year << ". \n"; // Ternary operator to determine days in February
+		cost = 1.10;
 	}
-	else if (month == 4 || month == 6 || month == 9 || month == 11) {
-
-		cout << "This month has 30 days in " << year << ". \n";
+	else if (weight > 2 and weight >= 6) {
+		cost = 2.20;
 	}
-	else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-
-		cout << "This month has 31 days in " << year << ". \n";
-
+	
+	else if (weight > 6 and weight >= 10) {
+		cost = 3.70;
 	}
-
+	
+	else if (weight > 10 and weight <= 20) {
+		cost = 4.80;
+	}
+	
+	double totalCost = (distance / 500) * cost;
+	cout << fixed << setprecision(2);
+	cout << "The shipping cost is: $" << totalCost << endl;
 	return 0;
 }
+
+
 
 
 
